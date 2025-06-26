38e5ac338fb16dcadb602f0848a945ceb495328c
fjzamora93 <fjzamora93@gmail.com>
2025-06-26 17:28:22 +0200

    update

--- lib/kidsandclouds/data/service/child_api_service.dart
+++ lib/kidsandclouds/data/service/child_api_service.dart
@@ -1,24 +1,35 @@
 import 'package:dio/dio.dart';
 import 'package:pruebakidsandclouds/core/helper/constants.dart';
 import 'package:pruebakidsandclouds/kidsandclouds/data/models/child.dart';
-import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 part 'child_api_service.g.dart';
 
-
-
-@RestApi(baseUrl: "${AppConstants.childMockUrl}")
+@RestApi(baseUrl: AppConstants.childMockUrl)
 abstract class ChildApiService {
   factory ChildApiService(Dio dio, {String baseUrl}) = _ChildApiService;
 
-
-  // Para obtener varios usuarios usar un query param con el número de resutlados esperados: 
-  // https://randomuser.me/api/?results=10
+  // Corregido: La API devuelve un objeto con "results", no directamente una lista
   @GET('/api')
-  Future<List<Child>> getChildren({
+  Future<ChildrenResponse> getChildren({
     @Query('results') int results = 10,
   });
+}
 
+// Clase para mapear la respuesta completa de la API
+@JsonSerializable()
+class ChildrenResponse {
+  final List<Child> results;
+  final Map<String, dynamic> info;
 
-  
+  const ChildrenResponse({
+    required this.results,
+    required this.info,
+  });
+
+  factory ChildrenResponse.fromJson(Map<String, dynamic> json) => 
+      _$ChildrenResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ChildrenResponseToJson(this);
 }
---
+++ lib/kidsandclouds/data/service/child_api_service.g.dart
@@ -0,0 +1,94 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'child_api_service.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ChildrenResponse _$ChildrenResponseFromJson(Map<String, dynamic> json) =>
+    ChildrenResponse(
+      results:
+          (json['results'] as List<dynamic>)
+              .map((e) => Child.fromJson(e as Map<String, dynamic>))
+              .toList(),
+      info: json['info'] as Map<String, dynamic>,
+    );
+
+Map<String, dynamic> _$ChildrenResponseToJson(ChildrenResponse instance) =>
+    <String, dynamic>{
+      'results': instance.results.map((e) => e.toJson()).toList(),
+      'info': instance.info,
+    };
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+
+class _ChildApiService implements ChildApiService {
+  _ChildApiService(this._dio, {this.baseUrl, this.errorLogger}) {
+    baseUrl ??= 'https://randomuser.me';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<ChildrenResponse> getChildren({int results = 10}) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'results': results};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ChildrenResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ChildrenResponse _value;
+    try {
+      _value = ChildrenResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(String dioBaseUrl, String? baseUrl) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
